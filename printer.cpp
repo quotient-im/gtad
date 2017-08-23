d26f7dd3f453b17322c42a946c8882ee866ea250
Kitsune Ral <Kitsune-Ral@users.sf.net>
2017-08-23 15:34:25 +0900

    More values exported to Mustache
    Also: used lambda3 made in kainjow/Mustache today that allows to switch escaping off.

--- printer.cpp
+++ printer.cpp
@@ -21,6 +21,8 @@
 #include "exception.h"
 
 #include <fstream>
+#include <algorithm>
+#include <locale>
 
 enum {
     CannotReadTemplateFile = PrinterCodes, CannotWriteToFile,
@@ -34,22 +36,39 @@ Printer::Printer(context_type&& context, const vector<string>& templateFileNames
     : _context(context)
 {
     // Enriching the context with "My Mustache library"
-    _context.set("embed", lambda {
-        [inputBasePath, this](const string& s) {
+    _context.set("@filePartial", lambda3 {
+        [inputBasePath, this](const string& s, const renderer2& render) {
             ifstream ifs { inputBasePath + s };
             if (!ifs.good())
             {
-                cerr << "#embed: Failed to open " << inputBasePath + s << endl;
+                cerr << "Failed to open file for a partial: "
+                     << inputBasePath + s << endl;
                 return "/* Failed to open " + inputBasePath + s + " */";
             }
             string embeddedTemplate;
             getline(ifs, embeddedTemplate, '\0'); // Won't work on files with NULs
-            // The below is not quite right - the calling code should render
-            // this template instead. Unfortunately, we now have a bug in Mustache
-            // that enforces HTML escaping upon the strings returned from lambdas.
-            mustache t { embeddedTemplate };
-            t.set_custom_escape([] (const string& s) { return s; });
-            return t.render(_context);
+            return render(embeddedTemplate, false);
+        }
+    });
+    _context.set("@cap", lambda2 {
+        [](const string& s, const basic_renderer<string>& render) {
+            return capitalizedCopy(render(s));
+        }
+    });
+    _context.set("@toupper", lambda2 {
+        [](string s, const basic_renderer<string>& render) {
+            s = render(s);
+            transform(s.begin(), s.end(), s.begin(),
+                      [] (char c) { return toupper(c, locale::classic()); });
+            return s;
+        }
+    });
+    _context.set("@tolower", lambda2 {
+        [](string s, const basic_renderer<string>& render) {
+            s = render(s);
+            transform(s.begin(), s.end(), s.begin(),
+                      [] (char c) { return tolower(c, locale::classic()); });
+            return s;
         }
     });
     for (const auto& templateFileName: templateFileNames)
@@ -75,10 +94,19 @@ Printer::Printer(context_type&& context, const vector<string>& templateFileNames
     }
 }
 
-inline void setList(data* data, const string& name, const list& list)
+template <typename ObjT>
+inline void setList(ObjT* object, const string& name, list&& list)
 {
-    (*data)[name + '?'] = !list.empty();
-    (*data)[name] = list;
+    if (list.empty())
+        (*object)[name + '?'] = true;
+    else
+    {
+        using namespace placeholders;
+        (*object)[name + '?'] = false;
+        for_each(list.begin(), list.end() - 1,
+                 bind(&data::set, _1, "hasMore", true));
+    }
+    (*object)[name] = list;
 }
 
 void Printer::print(const Model& model, const string& outputBasePath) const
@@ -89,13 +117,43 @@ void Printer::print(const Model& model, const string& outputBasePath) const
         list mImports;
         for (const auto& import: model.imports)
             mImports.emplace_back(import);
-        setList(&context, "imports", mImports);
+        setList(&context, "imports", std::move(mImports));
     }
     {
         list mClasses;
         for (const auto& callClass: model.callClasses)
-            mClasses.emplace_back(object { { "className", callClass.className} });
-        setList(&context, "callClasses", mClasses);
+        {
+            for (const auto& call: callClass.callOverloads)
+            {
+                object mClass { { "operationId", callClass.operationId }
+                              , { "httpMethod",  call.verb }
+                              , { "path", call.path }
+                              };
+                for (const auto& pp: {
+                    make_pair("pathParams", call.pathParams),
+                    make_pair("queryParams", call.queryParams),
+                    make_pair("headerParams", call.headerParams),
+                    make_pair("bodyParams", call.bodyParams),
+                    make_pair("allParams", call.collateParams())
+                })
+                {
+                    list mParams;
+                    for (const auto& param: pp.second)
+                    {
+                        object&& mParam { { "dataType", param.type }
+                                        , { "baseName", param.name }
+                                        , { "paramName", param.name } };
+                        mParams.emplace_back(mParam);
+                    }
+                    setList(&mClass, pp.first, move(mParams));
+                }
+                mClasses.emplace_back(move(mClass));
+            }
+        }
+        context.set("operations",
+            object { { "className", "!!!TODO:undefined!!!" },
+                     { "operation", mClasses } }
+        );
     }
     for (auto fileTemplate: _templates)
     {
