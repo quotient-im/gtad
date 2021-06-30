quotient-im/gtad

commit 3b872f7f0197dc1c253e8deffa63cd15314faebe

Mustache: make '_delimiter' work everywhere
The previous implementation of '_delimiter' only applied to the files
it was defined on, making it pretty much unusable since a lot of logic
is defined in 'gtad.yaml', and prepending every definition with the
delimiter override is impractical. The problem was that the main context
defined in 'gtad.yaml' was only passed when rendering files but not
when rendering types and imports. Moreover, Mustache does not pass it
over when traversing to/from partials so even file templates would need
a delimiter override at the beginning of each.

This commit adds GtadContext to all renderers, leaving the partials
problem to the next one. To save on copying things around, GtadContext
is only created anew for types (renderType() recurses and inheriting
the context from the previous level of recursion is undesirable) and
imports reuse the "main" one, push()ing and pop()ping data overlays
as needed. To further optimise and protect from mistakes, km::object
is used instead of type-erasing km::data for the top-level context
object throughout Printer.

This exposes a shortcoming of km::context::push/pop: to add an overlay
just for one rendering before returning a rendering result one has
to save that result to a temporary variable so that pop() could be
called. It would be great to have some convenience function that would
wrap a call km::mustache::render() into a push/pop pair. This is also
a matter of a separate commit.

printer.cpp
@@ -30,15 +30,15 @@ namespace km = kainjow::mustache;
 using km::object;
 using km::partial;
 
-inline string safeString(const Printer::context_type& data, const string& key,
-                         string defaultValue = {})
+inline string safeString(const Printer::context_type& contextObj,
+                         const string& key, string defaultValue = {})
 {
-    if (const auto* value = data.get(key))
-    {
-        if (value->is_string())
-            return value->string_value();
-        if (value->is_partial())
-            return value->partial_value()();
+    if (const auto it = contextObj.find(key); it != contextObj.end()) {
+        const auto& value = it->second;
+        if (value.is_string())
+            return value.string_value();
+        if (value.is_partial())
+            return value.partial_value()();
     }
     return defaultValue;
 }
@@ -70,7 +70,7 @@ class GtadContext : public km::context<string>
 
         GtadContext(Printer::fspath inputBasePath, const data* d)
             : context(d), inputBasePath(move(inputBasePath))
-        { }
+        {}
 
         const data* get_partial(const string& name) const override
         {
@@ -107,29 +107,29 @@ class GtadContext : public km::context<string>
         mutable unordered_map<string, data> filePartialsCache;
 };
 
-Printer::Printer(context_type&& contextData, fspath inputBasePath,
+Printer::Printer(context_type&& contextObj, fspath inputBasePath,
                  const fspath& outFilesListPath, const Translator& translator)
     : _translator(translator)
-    , _contextData(std::move(contextData))
-    , _delimiter(safeString(_contextData, "_delimiter"))
+    , _contextData(contextObj)
+    , _delimiter(safeString(contextObj, "_delimiter"))
     , _typeRenderer(makeMustache(
-          safeString(_contextData, "_typeRenderer", "{{>name}}")))
-    , _leftQuote(safeString(_contextData, "_leftQuote",
-                            safeString(_contextData, "_quote", "\"")))
-    , _rightQuote(safeString(_contextData, "_rightQuote",
-                             safeString(_contextData, "_quote", "\"")))
+          safeString(contextObj, "_typeRenderer", "{{>name}}")))
+    , _leftQuote(safeString(contextObj, "_leftQuote",
+                            safeString(contextObj, "_quote", "\"")))
+    , _rightQuote(safeString(contextObj, "_rightQuote",
+                             safeString(contextObj, "_quote", "\"")))
     , _inputBasePath(move(inputBasePath))
 {
     using km::lambda2;
     using km::renderer;
     // Enriching the context with "My Mustache library"
-    _contextData.set("_cap", lambda2 {
+    contextObj.emplace("_cap", lambda2 {
         [](const string& s, renderer render)
         {
             return capitalizedCopy(render(s, false));
         }
     });
-    _contextData.set("_toupper", lambda2 {
+    contextObj.emplace("_toupper", lambda2 {
         [](string s, renderer render) {
             s = render(s, false);
             transform(s.begin(), s.end(), s.begin(),
@@ -137,7 +137,7 @@ Printer::Printer(context_type&& contextData, fspath inputBasePath,
             return s;
         }
     });
-    _contextData.set("_tolower", lambda2 {
+    contextObj.emplace("_tolower", lambda2 {
         [](string s, renderer render) {
             s = render(s, false);
             transform(s.begin(), s.end(), s.begin(),
@@ -244,8 +244,17 @@ object Printer::renderType(const TypeUsage& tu) const
         qualifiedValues.emplace(to_string(i), mParamType["qualifiedName"]);
     }
 
-    return { { "name", _typeRenderer.render(values) }
-           , { "qualifiedName", _typeRenderer.render(qualifiedValues) }
+    km::data valuesData {values}, qualifiedValuesData {qualifiedValues};
+    GtadContext context {_inputBasePath, &_contextData};
+    context.push(&valuesData);
+    const auto renderedName = _typeRenderer.render(context);
+    context.pop();
+    context.push(&qualifiedValuesData);
+    const auto renderedQualifiedName = _typeRenderer.render(context);
+    context.pop();
+
+    return { { "name", renderedName }
+           , { "qualifiedName", renderedQualifiedName }
            , { "baseName", tu.baseName }
     };
 }
@@ -344,24 +353,38 @@ void Printer::print(const fspath& filePathBase, const Model& model) const
         return;
     }
 
-    auto contextData = _contextData;
-    contextData.set("filenameBase", filePathBase.filename().string());
-    contextData.set("basePathWithoutHost", model.basePath);
-    contextData.set("basePath", model.hostAddress + model.basePath);
-    setList(contextData, "imports", model.imports,
-            [this](const pair<string, string>& import) -> string {
+    GtadContext context{_inputBasePath, &_contextData};
+
+    object payloadObj {{"filenameBase", filePathBase.filename().string()}
+                      ,{"basePathWithoutHost", model.basePath}
+                      ,{"basePath", model.hostAddress + model.basePath}
+                      };
+    setList(payloadObj, "imports", model.imports,
+            [this, &context](const pair<string, string>& import) -> string {
                 if (import.first.empty() || import.second.empty()) {
                     cerr << "Warning: empty import, the emitted code will "
                             "likely be invalid"
                          << endl;
                     return {};
                 }
-                object importData {{"_", import.first}};
-                setList(importData, "segments", fspath(import.first));
+                static unordered_map<string, template_type> tmplCache {};
+                auto tmplIt = tmplCache.find(import.second);
+                if (tmplIt == tmplCache.end())
+                    tmplIt =
+                        tmplCache
+                            .emplace(import.second, makeMustache(import.second))
+                            .first;
+
+                object importContextObj {{"_", import.first}};
+                setList(importContextObj, "segments", fspath(import.first));
                 // This is where the import as collected from the API
                 // description is actually transformed to the language-specific
                 // import target (such as a C++ header file)
-                return makeMustache(import.second).render(importData);
+                km::data d {importContextObj};
+                context.push(&d);
+                const auto renderedImport = tmplIt->second.render(context);
+                context.pop();
+                return renderedImport;
             });
 
     // Unnamed schemas are only saved in the model to enable inlining
@@ -373,11 +396,11 @@ void Printer::print(const fspath& filePathBase, const Model& model) const
 
     auto&& mAllTypes = dumpAllTypes(namedSchemas); // Back-comp w/swagger
     if (!mAllTypes.empty())
-        contextData.set("allModels", mAllTypes);
+        payloadObj.emplace("allModels", mAllTypes);
 
     auto&& mTypes = dumpTypes(namedSchemas);
     if (!mTypes.empty())
-        contextData.set("models", mTypes);
+        payloadObj.emplace("models", mTypes);
 
     object mOperations;
     if (!model.callClasses.empty()) {
@@ -476,7 +499,7 @@ void Printer::print(const fspath& filePathBase, const Model& model) const
             mOperations.emplace("classname", "NOT_IMPLEMENTED");
             mOperations.emplace("consumesNonJson?", globalConsumesNonJson);
             mOperations.emplace("producesNonJson?", globalProducesNonJson);
-            contextData.set("operations", mOperations);
+            payloadObj.emplace("operations", mOperations);
         }
     }
     if (mTypes.empty() && mOperations.empty()) {
@@ -485,6 +508,8 @@ void Printer::print(const fspath& filePathBase, const Model& model) const
         return;
     }
 
+    km::data payload {payloadObj};
+    context.push(&payload);
     for (const auto& [fPath, fTemplate]:
          _translator.outputConfig(filePathBase, model)) {
         ofstream ofs{fPath};
@@ -492,9 +517,8 @@ void Printer::print(const fspath& filePathBase, const Model& model) const
             throw Exception(fPath.string() + ": Couldn't open for writing");
 
         cout << "Emitting " << fPath.string() << endl;
-        GtadContext c{_inputBasePath, &contextData};
         auto fullTemplate = makeMustache(fTemplate);
-        fullTemplate.render(c, ofs);
+        fullTemplate.render(context, ofs);
         if (fullTemplate.error_message().empty())
             _outFilesList << fPath.string() << endl;
         else

printer.h
@@ -11,14 +11,14 @@ class Translator;
 
 class Printer {
 public:
-    using context_type = kainjow::mustache::data;
+    using context_type = kainjow::mustache::object;
     using template_type = kainjow::mustache::mustache;
     using templates_type = std::vector<std::pair<template_type, template_type>>;
     using m_object_type = kainjow::mustache::object;
     using string = std::string;
     using fspath = std::filesystem::path;
 
-    Printer(context_type&& contextData, fspath inputBasePath,
+    Printer(context_type&& contextObj, fspath inputBasePath,
             const fspath& outFilesListPath, const Translator& translator);
     Printer(Printer&& p) = default;
 
@@ -27,7 +27,7 @@ public:
 
 private:
     const Translator& _translator;
-    context_type _contextData;
+    kainjow::mustache::data _contextData;
     string _delimiter;
     template_type _typeRenderer;
     string _leftQuote;

translator.cpp
@@ -238,25 +238,24 @@ Translator::Translator(const path& configFilePath, path outputDirPath,
         const auto pName = p.first.as<string>();
         if (p.second.IsScalar())
         {
-            env.set(pName, p.second.as<string>());
+            env.emplace(pName, p.second.as<string>());
             continue;
         }
         const auto pDefinition = p.second.asMap().front();
         const auto pType = pDefinition.first.as<string>();
         const YamlNode defaultVal = pDefinition.second;
         if (pType == "set")
-            env.set(pName, { data::type::list });
+            env.emplace(pName, data::type::list);
         else if (pType == "bool")
-            env.set(pName, { defaultVal.as<bool>() });
+            env.emplace(pName, defaultVal.as<bool>());
         else
-            env.set(pName, { defaultVal.as<string>() });
+            env.emplace(pName, defaultVal.as<string>());
     }
     const auto& partialsYaml = mustacheYaml["partials"].asMap();
     for (const auto& p: partialsYaml)
     {
-        env.set(p.first.as<string>(),
-                partial { [s = p.second.as<string>()] { return s; } });
-
+        env.emplace(p.first.as<string>(),
+                    partial {[s = p.second.as<string>()] { return s; }});
     }
 
     const auto& templatesYaml = mustacheYaml["templates"].asMap();

commit 34e095f501e17c606c8ac6690cb77bc76f21395f

Fix broken lambdas from "My Mustache library"
3b872f7f broke them.

printer.cpp
@@ -149,11 +149,36 @@ inline auto renderWithOverlay(const km::basic_mustache<StringT>& tmpl,
     return tmpl.render(ctx);
 }
 
+/// Enrich the context with "My Mustache library"
+inline Printer::context_type addLibrary(Printer::context_type contextObj)
+{
+    using km::lambda2;
+    using km::renderer;
+    contextObj.emplace("_cap", lambda2 {[](const string& s, renderer render) {
+                           return capitalizedCopy(render(s, false));
+                       }});
+    contextObj.emplace("_toupper", lambda2 {[](string s, renderer render) {
+                           s = render(s, false);
+                           transform(s.begin(), s.end(), s.begin(), [](char c) {
+                               return toupper(c, locale::classic());
+                           });
+                           return s;
+                       }});
+    contextObj.emplace("_tolower", lambda2 {[](string s, renderer render) {
+                           s = render(s, false);
+                           transform(s.begin(), s.end(), s.begin(), [](char c) {
+                               return tolower(c, locale::classic());
+                           });
+                           return s;
+                       }});
+    return contextObj;
+}
+
 Printer::Printer(context_type&& contextObj, fspath inputBasePath,
                  const fspath& outFilesListPath, string delimiter,
                  const Translator& translator)
     : _translator(translator)
-    , _contextData(contextObj)
+    , _contextData(addLibrary(contextObj))
     , _delimiter(move(delimiter))
     , _typeRenderer(
           makeMustache(safeString(contextObj, "_typeRenderer", "{{>name}}")))
@@ -163,31 +188,6 @@ Printer::Printer(context_type&& contextObj, fspath inputBasePath,
                              safeString(contextObj, "_quote", "\"")))
     , _inputBasePath(move(inputBasePath))
 {
-    using km::lambda2;
-    using km::renderer;
-    // Enriching the context with "My Mustache library"
-    contextObj.emplace("_cap", lambda2 {
-        [](const string& s, renderer render)
-        {
-            return capitalizedCopy(render(s, false));
-        }
-    });
-    contextObj.emplace("_toupper", lambda2 {
-        [](string s, renderer render) {
-            s = render(s, false);
-            transform(s.begin(), s.end(), s.begin(),
-                      [] (char c) { return toupper(c, locale::classic()); });
-            return s;
-        }
-    });
-    contextObj.emplace("_tolower", lambda2 {
-        [](string s, renderer render) {
-            s = render(s, false);
-            transform(s.begin(), s.end(), s.begin(),
-                      [] (char c) { return tolower(c, locale::classic()); });
-            return s;
-        }
-    });
     if (!outFilesListPath.empty())
     {
         _outFilesList.open(_translator.outputBaseDir() / outFilesListPath);